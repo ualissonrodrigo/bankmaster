bf3469b3adf385cf18bb00d451d01943913951b9
Ualisson Rodrigo <ualissontourniquet@hotmail.com>
2021-05-31 23:45:55 -0300

    improving forms to brazilian real

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    def normalize_money(x)
+        ('%.2f' % x.to_s.gsub(/\D/, '')).to_f / 100        
+    end
 end
